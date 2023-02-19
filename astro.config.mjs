172fae3c4abb085589b550ea8854718b537e687a
Giuliopime <60524738+Giuliopime@users.noreply.github.com>
2023-02-19 17:01:49 +0100

    feat: vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import cloudflare from "@astrojs/cloudflare";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare()
+  adapter: vercel()
 });
